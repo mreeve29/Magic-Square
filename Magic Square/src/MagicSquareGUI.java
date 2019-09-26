52ef92567ed389c2107d84e3a20fc85da945cc20
Michael Reeve <michael.s.reeve@gmail.com>
2019-09-26 12:36:15 -0400

    initial commit

---
+++ Magic Square/src/MagicSquareGUI.java
@@ -0,0 +1,13 @@
+import BreezySwing.*;
+import javax.swing.*;
+
+public class MagicSquareGUI extends GBFrame{
+	public MagicSquareGUI() {
+	}
+	
+	public static void main(String[] args) {
+		MagicSquareGUI frm = new MagicSquareGUI();
+		frm.setVisible(true);
+		frm.setTitle("");
+	}
+}
